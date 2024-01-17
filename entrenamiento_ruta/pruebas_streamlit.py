7257aa6c3ae2dfb59a6c4d7b0b16ff467ba08afb
realpabloperez <145840452+realpabloperez@users.noreply.github.com>
2024-01-17 14:22:07 +0100

    ruta con coordenadas en array
    siguiente paso es leer de un .KML en vez de hardcodear las coordenadas en un array. Nota: el .KML de GoogleMaps pone las coordenadas al revés, además de incluir la profundidad ",0" que streamlit no usa para nada y tenemos que eliminar o ignorar.

--- entrenamiento_ruta/pruebas_streamlit.py
+++ entrenamiento_ruta/pruebas_streamlit.py
@@ -1,25 +1,45 @@
-import folium
 import streamlit as st
+import folium
 from streamlit_folium import st_folium
+import pandas as pd
 
-# Center on Liberty Bell, add markers
-m = folium.Map(location=[39.46939,-0.35902], zoom_start=16)
+# List of coordinates (latitude, longitude)
+coordinates = [
+    [39.46939, -0.35902],
+    [39.46949, -0.35889],
+    [39.46958, -0.35876],
+    [39.46962, -0.35871],
+    [39.46969, -0.35865],
+    [39.4698, -0.35858],
+    [39.46992, -0.35849],
+    [39.47013, -0.35842],
+    [39.47014, -0.3584],
+    [39.47015, -0.35839],
+    [39.47018, -0.35837],
+    [39.47019, -0.35836],
+    [39.47021, -0.35835],
+    [39.47024, -0.35834],
+    [39.47027, -0.35834],
+    [39.4703, -0.35834],
+    [39.47033, -0.35837],
+    [39.47035, -0.35839],
+    [39.47038, -0.35834],
+    [39.47043, -0.35827],
+    [39.47048, -0.35822],
+]
 
-# Marker for Liberty Bell
-folium.Marker(
-    [39.46939,-0.35902], popup="01", tooltip="Localización"
-).add_to(m)
+# Convert the coordinates to a DataFrame
+df = pd.DataFrame(coordinates, columns=["LATITUDE", "LONGITUDE"])
 
-second_location = [39.46949,-0.35889]
-folium.Marker(
-    second_location, popup="02", tooltip="Localización"
-).add_to(m)
+# Display the DataFrame using st.dataframe
+st.dataframe(df)
 
-third_location = [39.46958,-0.35876,]
-folium.Marker(
-    third_location, popup="03", tooltip="Localización"
-).add_to(m)
+# Create a Folium map to add markers
+m = folium.Map(location=[coordinates[0][0], coordinates[0][1]], zoom_start=15)
 
-# Call to render Folium map in Streamlit
-# Don't get any data back from the map (so that it won't rerun the app when the user interacts)
-st_folium(m, width=725, returned_objects=[])
+# Add markers to the Folium map using the DataFrame
+for index, row in df.iterrows():
+    folium.Marker(location=[row["LATITUDE"], row["LONGITUDE"]]).add_to(m)
+
+# Display the Folium map using st_folium
+st_folium(m)
