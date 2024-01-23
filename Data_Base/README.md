e9fb4135a8c5f6e37d49e78fed07289221aa82e0
albertodegeapla <146119880+albertodegeapla@users.noreply.github.com>
2024-01-23 22:51:52 +0100

    UPDATE GENERADOR
    GENERADOR DE PERSONAS Y COCHES DONE

--- Data_Base/README.md
+++ Data_Base/README.md
@@ -1,13 +1,7 @@
 # Fuentes de datos
 
-## User Generator.
-La frecuencia de hombre y mujeres que usan blablacar se ha escogido en base a esta noticia:
-https://blog.blablacar.es/newsroom/noticias/blablacar-alcanza-los-ocho-millones-de-usuarios-en-espana#:~:text=La%20edad%20media%20de%20la,%25%20hombres%2C%2043%25%20mujeres.
+## User y Car Generator.
+Dentro de la carpeta personas y coches, tenemos los generadores de las personas y los coches. Ahí podemos generar todos los coches y personas que deseemos debido al loop que podemos encontrar al final de cada script.
 
-La distribucion de la edad de los usuarios está basada en la siguiente estadistica.
-https://es.statista.com/estadisticas/628281/edad-de-los-usuarios-de-blablacar-en-espana/
+## Path generator
 
-Los csvs del nombre de hombres, mujeres y apellidos con sus respectivas frecuencias
-https://github.com/marcboquet/spanish-names/tree/master
-
-## Car Generator
---
+++ Data_Base/personas y coches/generador_personas.py
@@ -0,0 +1,57 @@
+import random
+
+def generar_id_persona():
+    return random.randint(10000, 99999)
+
+def cargar_txt(archivo):
+    with open(archivo, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file]
+
+def generar_nombres():
+    nombre = cargar_txt('/Users/albertodegeapla/Desktop/DATA-PROJECT-II/Data_Base/personas y coches/nombre.txt')
+    return random.choice(nombre)
+
+def generar_primer_apellido():
+    primer_apellido = cargar_txt('/Users/albertodegeapla/Desktop/DATA-PROJECT-II/Data_Base/personas y coches/apellido.txt')
+    return random.choice(primer_apellido)
+
+def generar_segundo_apellido():
+    segundo_apellido = cargar_txt('/Users/albertodegeapla/Desktop/DATA-PROJECT-II/Data_Base/personas y coches/apellido.txt')
+    return random.choice(segundo_apellido)
+
+def generar_dni():
+    numeros = random.randint(10000000, 99999999)
+    letra = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    return f"{numeros}-{letra}"
+
+def generar_edad():
+    random.randint(18, 75)
+
+def generar_cartera():
+    return round(random.uniform(100, 10000), 2)
+
+def generar_persona():
+    id_persona = generar_id_persona
+    nombre = generar_nombres
+    primer_apellido = generar_primer_apellido
+    segundo_apellido = generar_segundo_apellido
+    dni = generar_dni
+    edad = generar_edad
+    cartera = generar_cartera
+
+    persona = {
+        'ID_persona':id_persona,
+        'Nombre':nombre,
+        'Primer apellido':primer_apellido,
+        'Segundo apellido':segundo_apellido,
+        'DNI':dni,
+        'Edad':edad,
+        'Cartera':cartera
+    }
+
+    return persona
+
+#SOLO DEVOLVERÁ UNA PERSONA PARA ESTE VIERNES
+for _ in range(1):
+    persona_generada = generar_persona()
+    print(persona_generada)
