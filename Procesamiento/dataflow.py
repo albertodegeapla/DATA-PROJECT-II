1c1024d0b13d3288d1ba4e8355d3bf7fe757fde6
pepe54aguilar <pep343000@gmail.com>
2024-01-25 23:42:40 +0100

    juntar ambos flujos en uno
    He hecho un script que recibe los mensajes del generaador desde los dos diferentes topics y los junta en una tuberia.
    ademas por separado he hecho pruebas en local para insertarlos en bigquery  y funciona

--- Procesamiento/dataflow.py
+++ Procesamiento/dataflow.py
@@ -25,8 +25,57 @@ def decode_message(message):
             print(f"Error decoding message: {e}")
             return None
 
-with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
-    data = (p 
+def run_local():
+    options = PipelineOptions(streaming=True)
+    with beam.Pipeline(options=options) as p:
+        p_peaton = (p
+                        | "ReadFromPubSubPeaton" >> beam.io.ReadFromPubSub(subscription='projects/genuine-essence-411713/subscriptions/ruta_persona-sub')
+                        | "DecodeMessagePeaton" >> beam.Map(decode_message)
+                        | "addTuplePeaton" >> beam.Map(lambda x: ("peaton", x))
+                        | "WindowIntoPeaton" >> beam.WindowInto(beam.window.FixedWindows(10)) 
+                    )
+        p_coche =   (p 
+                        | "ReadFromPubSubCoche" >> beam.io.ReadFromPubSub(subscription='projects/genuine-essence-411713/subscriptions/ruta_coche-sub')
+                        | "DecodeMessageCoche" >> beam.Map(decode_message)
+                        | "addTupleCoche" >> beam.Map(lambda x: ("coche", x))
+                        | "WindowIntoCoche" >> beam.WindowInto(beam.window.FixedWindows(10)) 
+                    )
+        
+        data = ((p_peaton, p_coche)
+                        | "groupByKey" >> beam.CoGroupByKey()
+                        | "print" >> beam.Map(print))
+        
+
+
+
+
+'''                     | "WriteToBigQuery" >> beam.io.WriteToBigQuery(
+                            table='genuine-essence-411713:blablacar2.ruta_coche',
+                            schema='{"ID_coche":"STRING", "Marca":"STRING", "Matricula":"STRING", "Plazas":"INTEGER","Precio":"FLOAT","hora_salida":"STRING", "ruta_coche":"STRING"}',
+                            create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
+                            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+                        )
+
+                        | "WriteToBigQuery" >> beam.io.WriteToBigQuery(
+                            table='genuine-essence-411713:blablacar2.ruta_persona',
+                            schema='{"ID_persona":"STRING", "Nombre":"STRING", "Cartera":"FLOAT","hora_salida":"STRING", "ruta_perosna":"STRING"}',
+                            create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
+                            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+                        )                     
+'''
+        
+    
+'''def run_GCP():
+    with beam.Pipeline(options=PipelineOptions(
+        streaming=True,
+        # save_main_session=True
+        project=project_id,
+        runner="DataflowRunner",
+        temp_location=f"gs://{bucket_name}/tmp",
+        staging_location=f"gs://{bucket_name}/staging",
+        region="europe-west1"
+    )) as p:
+        (p 
             | "ReadFromPubSub" >> beam.io.ReadFromPubSub(subscription='projects/genuine-essence-411713/subscriptions/ruta_persona-sub')
             | "DecodeMessage" >> beam.Map(decode_message)
             | "WriteToBigQuery" >> beam.io.WriteToBigQuery(
@@ -35,5 +84,13 @@ with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
                 create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
                 write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
             )
-    )    
+        )  '''
 
+
+if __name__ == '__main__':
+
+    logging.getLogger().setLevel(logging.INFO)
+
+    logging.info("The process started")
+    
+    run_local()
